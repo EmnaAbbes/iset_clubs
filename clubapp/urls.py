b8373ef0b09e4f45512fb1c0d4a1ec77eb62ed27
EmnaAbbes <emna.abbes5@gmail.com>
2023-09-29 11:04:25 +0100

    new commit

--- clubapp/urls.py
+++ clubapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('',views.index,name="index"),
+    path('pdf',views.CreatePDF,name="pdf"),
+    path('htmlpage',views.htmlpage,name="html")
+
 
 ]
